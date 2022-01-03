021d8c9923b75c0f726e9a4860f969d09f7f757d
Maciej Wołczyk <raihid888@gmail.com>
2022-01-03 12:26:21 +0100

    Triplet experiments

--- continualworld/tasks.py
+++ continualworld/tasks.py
@@ -13,4 +13,21 @@ TASK_SEQS = {
     ],
 }
 
+SUPPLEMENTARY_TRIPLETS = [
+    # Good ABC triplets
+    ['push-v1', 'window-close-v1', 'hammer-v1'],
+    ['hammer-v1', 'window-close-v1', 'faucet-close-v1'],
+    # ??? ABC triplets
+    ['stick-pull-v1', 'push-back-v1', 'push-wall-v1'],
+    ['push-wall-v1', 'shelf-place-v1', 'push-back-v1'],
+    ['faucet-close-v1', 'shelf-place-v1', 'push-back-v1'],
+    ['stick-pull-v1', 'peg-unplug-side-v1', 'stick-pull-v1'],
+    ['window-close-v1', 'handle-press-side-v1', 'peg-unplug-side-v1'],
+    ['faucet-close-v1', 'shelf-place-v1', 'peg-unplug-side-v1'],
+    # Bad triplets
+    ['peg-unplug-side-v1', 'push-v1', 'push-wall-v1'],
+    ['window-close-v1', 'hammer-v1', 'push-v1'],
+    ['push-back-v1', 'push-wall-v1', 'push-v1'],
+]
+
 TASK_SEQS["CW20"] = TASK_SEQS["CW10"] + TASK_SEQS["CW10"]
---
+++ mrunner_exps/maciej/mw_306_bc_triplets.py
@@ -0,0 +1,35 @@
+from mrunner.helpers.specification_helper import create_experiments_helper
+
+from mrunner_utils import combine_config_with_defaults
+from continualworld.tasks import SUPPLEMENTARY_TRIPLETS
+
+name = globals()["script"][:-3]
+config = {
+    "run_kind": "cl",
+    "logger_output": ["tsv", "neptune"],
+}
+config = combine_config_with_defaults(config)
+
+params_grid = [
+    {
+        "seed": list(range(20)),
+        "task_list": SUPPLEMENTARY_TRIPLETS,
+        "cl_method": ["episodic_replay"],
+        "regularize_critic": [False, True],
+        "episodic_mem_per_task": [10000],
+        "cl_reg_coef": [100],
+        "episodic_batch_size": [128],
+        "clipnorm": [None],
+    },
+]
+
+experiments_list = create_experiments_helper(
+    experiment_name=name,
+    project_name="pmtest/continual-learning-2",
+    script="python3 mrunner_run.py",
+    python_path=".",
+    tags=[name, "v6"],
+    base_config=config,
+    params_grid=params_grid,
+)
+
