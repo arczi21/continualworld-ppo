8bd31b3209f1974a6556244ba71c26dd167e67ef
Maciej Wołczyk <raihid888@gmail.com>
2022-10-12 15:41:40 +0200

    Fix gym version

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 
 # Required dependencies
 required = [
+    "gym<0.26",
     "tensorflow>=2.0",
     "mujoco-py<2.1,>=2.0",
     "metaworld @ git+https://github.com/rlworkgroup/metaworld.git@18118a28c06893da0f363786696cc792457b062b#egg=metaworld",
